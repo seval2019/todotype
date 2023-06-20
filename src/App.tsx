a70033861ee30567aeee54f7b4b7831875a211dd
Seval Senturk <seval.senturk@tgs.aero>
2023-06-20 08:31:38 +0300

    small todo type project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, FC } from "react";
+import "./App.css";
+import { useState } from "react";
+import { todoType } from "./apptypes";
+import TodoItem from "./TodoItem";
 
-function App() {
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [workDay, setWorkDay] = useState<number>(0);
+  const [todoList, setTodoList] = useState<todoType[]>([]);
+
+  console.log(todoList);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else if (event.target.name === "workDay") {
+      setWorkDay(Number(event.target.value));
+    }
+    console.log(task, workDay);
+  };
+
+  const addNewTask = (): void => {
+    const newTask = { taskName: task, workDay: workDay };
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setWorkDay(0);
+  };
+
+  const deleteTask = (nameToDelete: string) => {
+    setTodoList(todoList.filter((task)=>{
+      return task.taskName !== nameToDelete
+    }))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="maincard">
+        <input
+          className="maincardinput"
+          type="text"
+          value={task}
+          name="task"
+          placeholder="Taskınızı Giriniz..."
+          onChange={handleChange}
+        />
+        <input
+          className="maincardinput"
+          type="number"
+          value={workDay}
+          name="workDay"
+          placeholder="Kaç günde tamamlanmalı"
+          onChange={handleChange}
+        />
+        <button className="maincardbutton" onClick={addNewTask}>
+          Yeni task ekle
+        </button>
+      </div>
+      <div className="todocard">
+        {todoList.map((task: todoType, index: number) => {
+          return <TodoItem key={index} task={task} deleteTask={deleteTask} />;
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/apptypes.ts
@@ -0,0 +1,6 @@
+export type todoType={
+    taskName:string;
+    workDay:number;
+  }
+
+ 
